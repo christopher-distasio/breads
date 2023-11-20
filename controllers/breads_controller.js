67f179b6a270ff5cc54c4ad2cef4eee202095bb8
Chris DiStasio <stahzz1970@gmail.com>
2023-11-20 16:22:09 -0600

    Completes Part 7 solution

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,11 +4,9 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('index', {
-        breads: Bread
-      }
-    )
-  // res.send(Bread)
+  res.render('index', {
+    breads: Bread
+  })
 })
 
 // NEW
@@ -16,11 +14,19 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
+      bread: Bread[req.params.arrayIndex],
       index: req.params.arrayIndex,
     })
   } else {
@@ -32,13 +38,26 @@ breads.get('/:arrayIndex', (req, res) => {
 // CREATE
 breads.post('/', (req, res) => {
   console.log(req.body)
-  if(req.body.hasGluten === 'on') {
+  if (req.body.hasGluten === 'on') {
     req.body.hasGluten = 'true'
   } else {
     req.body.hasGluten = 'false'
   }
   Bread.push(req.body)
   res.redirect('/breads')
+})
+
+
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 // DELETE
--- views/show.jsx
+++ views/show.jsx
@@ -13,7 +13,10 @@ function Show({ bread, index }) {
         have gluten.
       </p>
       <img src={bread.image} alt={bread.name} />
-      <br />
+
+      <a href={`/breads/${index}/edit`}>
+        <button>Edit</button>
+      </a>
 
       <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
