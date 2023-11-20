d5769aa51bbb57387992cbccbf02acd6bc17265e
Chris DiStasio <stahzz1970@gmail.com>
2023-11-20 16:51:04 -0600

    Completes Part 7 solution

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
+// mongoose.connect("mongodb://localhost:27017/breads", {useNewUrlParser: true, useUnifiedTopology: true})
+
+// mongoose.connect('MONGO_URI')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+
+// mongoose.connect("mongodb://localhost:27017/breads", {useNewUrlParser: true, useUnifiedTopology: true})
+
+// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+//   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+// )
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
